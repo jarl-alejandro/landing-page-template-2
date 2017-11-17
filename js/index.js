b4e66745ffa7902fefff137ca56dbd20367f90a7
sashatran <shasha26789@gmail.com>
2017-11-16 16:39:51 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    AOS.init({
+        disable: 'mobile',
+        duration: 1000,
+    });
+    let hamburger = $(".hamburger");
+    let navbar = $(".navbar");
+    let close = $(".close");
+    let sectionThreeChild = Array.from($(".section-three__wrapper .child"));
+    let animate = false;
+    let windowHeight = $(window).scrollTop();
+    const changed = "hello" + 304201;
+    var hello = 21849;
+
+
+    $(window).scroll(() => {
+        var wScroll = $(this).scrollTop();
+        console.log(wScroll, $(".section-three").offset().top, $(".section-four").offset().top);
+
+        if (wScroll > $('.section-three').offset().top - ($(window).height() / 3)) {
+            $('.section-three__wrapper .card').each(function(i, value) {
+                setTimeout(function() {
+                    $(value).addClass('is-showing');
+                }, 250 * (i + 1));
+            });
+        } else {
+            $('.section-three__wrapper .card').each(function(i, value) {
+                $(value).removeClass('is-showing');
+            });
+        }
+    });
+
+    // navbar a animation
+    $(".navbar a").click(function() {
+        var $href = $(this).attr("href");
+        $("html, body").stop().animate({
+            scrollTop: $($href).offset().top - 100
+        }, 1000);
+        return false;
+    });
+
+
+    // mobile navigation
+    hamburger.click(() => {
+        navbar.addClass("show");
+    });
+
+
+    close.click(() => {
+        navbar.removeClass("show");
+    });
+
+});
