c0290dc8d7bdc35b28085474e3810312ab2c9cd9
sashatran <shasha26789@gmail.com>
2017-11-16 17:07:13 -0800

    update css, js

--- dist/index.js
+++ dist/index.js
@@ -10,26 +10,24 @@ $(document).ready(function() {
     let sectionThreeChild = Array.from($(".section-three__wrapper .child"));
     let animate = false;
     let windowHeight = $(window).scrollTop();
-    const changed = "hello" + 304201;
-    var hello = 21849;
 
 
-    $(window).scroll(() => {
-        var wScroll = $(this).scrollTop();
-        console.log(wScroll, $(".section-three").offset().top, $(".section-four").offset().top);
+    // $(window).scroll(() => {
+    //     var wScroll = $(this).scrollTop();
+    //     console.log(wScroll, $(".section-three").offset().top, $(".section-four").offset().top);
 
-        if (wScroll > $('.section-three').offset().top - ($(window).height() / 3)) {
-            $('.section-three__wrapper .card').each(function(i, value) {
-                setTimeout(function() {
-                    $(value).addClass('is-showing');
-                }, 250 * (i + 1));
-            });
-        } else {
-            // $('.section-three__wrapper .card').each(function(i, value) {
-            //     $(value).removeClass('is-showing');
-            // });
-        }
-    });
+    //     if (wScroll > $('.section-three').offset().top - ($(window).height() / 3)) {
+    //         $('.section-three__wrapper .card').each(function(i, value) {
+    //             setTimeout(function() {
+    //                 $(value).addClass('is-showing');
+    //             }, 250 * (i + 1));
+    //         });
+    //     } else {
+    //         // $('.section-three__wrapper .card').each(function(i, value) {
+    //         //     $(value).removeClass('is-showing');
+    //         // });
+    //     }
+    // });
 
     // navbar a animation
     $(".navbar a").click(function() {
