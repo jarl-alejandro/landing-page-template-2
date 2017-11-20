db10365fa4c9a8b9dc8ff32d129914cc3a9519ce
sashatran <shasha26789@gmail.com>
2017-11-20 10:42:12 -0800

    updat js

--- js/index.js
+++ js/index.js
@@ -13,24 +13,6 @@ $(document).ready(function() {
     const changed = "hello" + 304201;
     var hello = 21849;
 
-
-    // $(window).scroll(() => {
-    //     var wScroll = $(this).scrollTop();
-    //     console.log(wScroll, $(".section-three").offset().top, $(".section-four").offset().top);
-
-    //     if (wScroll > $('.section-three').offset().top - ($(window).height() / 3)) {
-    //         $('.section-three__wrapper .card').each(function(i, value) {
-    //             setTimeout(function() {
-    //                 $(value).addClass('is-showing');
-    //             }, 250 * (i + 1));
-    //         });
-    //     } else {
-    //         $('.section-three__wrapper .card').each(function(i, value) {
-    //             $(value).removeClass('is-showing');
-    //         });
-    //     }
-    // });
-
     // navbar a animation
     $(".navbar a").click(function() {
         var $href = $(this).attr("href");
